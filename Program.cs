25b0b24f60535eae88e7422940eb639f3ac7c491
rinsh4dd <rinshadcontacts@gmail.com>
2025-09-15 10:51:27 +0530

    ADO.NET

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connstr = "Server=RINSHAD\\SQLEXPRESS; Database=ADO_DOTNET; Integrated Security=True; TrustServerCertificate=True;";
+
+        using (SqlConnection conn = new SqlConnection(connstr))
+        {
+            conn.Open();
+            Console.WriteLine("Connect ADO.NET To Data Source Successfully");
+
+            String sql = "SELECT * FROM Students";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["Id"]}, {reader["Name"]}");
+            }
+        }
+    }
+}
